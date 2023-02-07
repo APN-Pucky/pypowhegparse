b245ac44f04e722348a96af9272f9335cc35bd53
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-02-07 16:38:30 +0100

    Add some phdplots

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
+        "pytopdrawer",
         "uncertainties",
         "numpy",
         "matplotlib",
